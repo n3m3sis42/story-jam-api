c583e4b358e1338a626cfbb4d0f54885cc960220
github username <github email address>
2017-08-22 17:33:58 -0400

    added Spread.build_board

--- app/models/spread.rb
+++ app/models/spread.rb
@@ -6,4 +6,70 @@ class Spread < ApplicationRecord
   has_many :stories, through: :jam
   has_many :user_jams, through: :jam
   has_one :chat, through: :jam
-end
+
+  def build_board
+    info = self.type.data[:order_and_position]
+    card_spreads = self.card_spreads.order(:order)
+
+    # iterate over the card_spreads
+    # for each element, use the index to get the position and z-depth from the card-spread.
+    # assign the empty-board where the board key == the element position.
+
+    card_spreads.each_with_index do |card_spreads, index|
+      position = info[:order_and_position][index][:position]
+      z = info[:order_and_position][index][:z]
+      card = Card.find(card_spreads.card_id)
+      flipped = card_spreads.flipped
+
+      board[position] = {
+        flipped: flipped,
+        data: {
+          id: card.id,
+          name: card.name,
+          description: card.description,
+          flipped_name: card.flipped_name,
+          flipped_description: card.flipped_description,
+          image_url: card.image_url,
+          flipped_image_url: card.flipped_image_url,
+          category_id: card.category_id
+        }
+      }
+    end
+
+    board = {
+      '0': nil,
+      '1': nil,
+      '2': nil,
+      '3': nil,
+      '4': nil,
+      '5': nil,
+      '6': nil,
+      '7': nil,
+      '8': nil,
+      '9': nil,
+      '10': nil,
+      '11': nil,
+      '12': nil,
+      '13': nil,
+      '14': nil,
+      'A': nil,
+      'B': nil,
+      'C': nil,
+      'D': nil,
+      'E': nil,
+      'F': nil,
+      'G': nil,
+      'H': nil,
+      'I': nil,
+      'J': nil,
+      'K': nil,
+      'L': nil,
+      'M': nil,
+      'N': nil,
+      'O': nil,
+    }
+
+    return board
+  end # end build_board
+
+end # end class
--- app/models/type.rb
+++ app/models/type.rb
@@ -5,14 +5,14 @@ class Type < ApplicationRecord
     "Once Upon a Time": {
       card_num: 8,
       order_and_position: {
-        "1": {meaning: "The protagonist, before the story", position: 6, z: 0},
-        "2": {meaning: "The status quo", position: 6, z: 1},
-        "3": {meaning: "What makes the situation unstable", position: 1, z: 0},
-        "4": {meaning: "What makes them resist getting involved", position: 11, z: 0},
-        "5": {meaning: "What overcomes that resistance", position: 5, z: 0},
-        "6": {meaning: "What propels the protagonist into action", position: 7, z: 0},
-        "7": {meaning: "The direction the protagonist is pushed", position: 8, z: 0},
-        "8": {meaning: "The apparent goal", position: 9, z: 0}
+        "1": {meaning: "The protagonist, before the story", position: 6},
+        "2": {meaning: "The status quo", position: G},
+        "3": {meaning: "What makes the situation unstable", position: 1},
+        "4": {meaning: "What makes them resist getting involved", position: 11},
+        "5": {meaning: "What overcomes that resistance", position: 5},
+        "6": {meaning: "What propels the protagonist into action", position: 7},
+        "7": {meaning: "The direction the protagonist is pushed", position: 8},
+        "8": {meaning: "The apparent goal", position: 9}
       }
     }
   }
